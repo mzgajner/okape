27a9ca9e1fcbe66619b1d4125d9bf17c0055e71b
mzgajner <mato@zgajner.com>
2020-07-19 22:58:19 +0200

    Render pickup days on last step

--- src/components/5-ShowResults.vue
+++ src/components/5-ShowResults.vue
@@ -1,23 +1,38 @@
 <template>
   <div>
-    <h1>Results are here!</h1>
-    {{ municipality }}
-    {{ street }}
-    {{ buildingType }}
-    {{ pickupDays }}
+    <h1 class="step-title">{{ street }}</h1>
+
+    <ul>
+      <li
+        v-for="pickup in pickups"
+        :key="pickup.type"
+      >
+        <span class="day-label" :style="`background: ${pickup.color}`">{{ pickup.type }}</span>
+        - {{ pickup.time }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { municipality, street, buildingType, pickupDays } from '../routes';
+import { generatePickups } from '../helpers';
 
 export default Vue.extend({
   name: 'ShowResults',
   props: { municipality, street, buildingType, pickupDays },
+  computed: {
+    pickups() {
+      const [regularPickupDay, organicPickupDay] = this.pickupDays.split('+');
+      return generatePickups(regularPickupDay!);
+    }
+  }
 });
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.day-label {
+  padding: 0 4px;
+}
 </style>
