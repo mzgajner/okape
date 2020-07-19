ecf7e54ae04f8a459b2056c2ab915e5519b7b146
mzgajner <mato@zgajner.com>
2020-07-19 22:22:20 +0200

    Extract styling to separate file

--- src/App.vue
+++ src/App.vue
@@ -6,118 +6,19 @@
       title="Garbage Truck by Karla Design from the Noun Project."
     />
 
-    <router-view></router-view>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-import { Pickup, generatePickups } from './helpers';
-
-const DAYS_LOCATIVE:{[key:string]: string} = {
-  'ponedeljek': 'ponedeljek',
-  'torek': 'torek',
-  'sreda': 'sredo',
-  'četrtek': 'četrtek',
-  'petek': 'petek',
-}
-
 export default Vue.extend({
-  name: "app",
-  // created() {
-  //   this.municipality = localStorage.getItem('municipality');
-  //   this.street = localStorage.getItem('street');
-  // },
-  // computed: {
-  //   municipalities(): string[] {
-  //     return Object.keys(this.schedule);
-  //   },
-  //   streets(): string[] {
-  //     if (this.municipality) {
-  //       return Object.keys(this.schedule[this.municipality] || {});
-  //     } else {
-  //       return []
-  //     }
-  //   },
-  //   garbagePickupDay(): string|null {
-  //     if (this.municipality && this.street) {
-  //       return this.schedule[this.municipality][this.street];
-  //     } else {
-  //       return null;
-  //     }
-  //   }
-  // },
-  // watch: {
-  //   garbagePickupDay(newValue) {
-  //     if (newValue) {
-  //       localStorage.setItem('municipality', this.municipality!);
-  //       localStorage.setItem('street', this.street!);
-  //       this.pickups = generatePickups(newValue);
-  //     } else {
-  //       localStorage.clear();
-  //       this.pickups = [];
-  //     }
-  //   },
-  // },
-  // methods: {
-  //   reset() {
-  //     this.municipality = null;
-  //     this.street = null;
-  //   },
-  // },
+  name: "App",
 });
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-html, body {
-  height: 100%;
-}
-body {
-  color: #35495E;
-  display: flex;
-  font-size: 18px;
-  justify-content: center;
-  margin: 0;
-  padding: 1rem;
-}
-
-#app {
-  align-items: center;
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  font-family: Courier New, Helvetica, Arial, sans-serif;
-  justify-content: center;
-  max-width: 400px;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-.logo {
-  margin: 1rem 0;
-  width: 100px;
-}
-
-.dropdown {
-  margin: 0.5rem 0;
-}
-
-.notice {
-  margin: 1rem 0;
-}
-
-li:not(:last-child) {
-  margin-bottom: 0.5rem;
-}
-
-.reset-link {
-  /* font-size: 0.75rem; */
-  color: grey;
-}
-</style>
+<style src="./main.css"></style>
