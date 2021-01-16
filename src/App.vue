6b984674717314273d36c9637677fd47abab5114
mzgajner <mato@zgajner.com>
2021-01-16 19:16:21 +0100

    Remove existing style sheet

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
-  <div id="app">
-    <img
-      class="logo"
-      src="./assets/garbage_truck.svg"
-      title="Garbage Truck by Karla Design from the Noun Project."
-    />
+  <div class="flex bg-purple-50">
+    <div class="flex items-center flex-col justify-center w-96 max-w-full h-screen mx-auto text-center ">
+      <img
+        class="m-4 w-28"
+        src="./assets/garbage_truck.svg"
+        title="Garbage Truck by Karla Design from the Noun Project."
+      />
 
-    <div class="container">
-      <router-view></router-view>
+      <router-view class="min-h-64 w-full"></router-view>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App',
-})
-</script>
-
-<style src="./main.css"></style>
